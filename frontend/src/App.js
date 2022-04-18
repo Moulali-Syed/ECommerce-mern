a2db8d82dc0f04ebc964d781abfee26a8f94c56c
Moulali Syed <syedmoulali1997@gmail.com>
2022-04-18 06:53:47 +0530

    create-react-app initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="https://ecommerce.com">ECommerce</a>
+      </header>
+      <main>List products</main>
+    </div>
+  );
+}
+
+export default App;
